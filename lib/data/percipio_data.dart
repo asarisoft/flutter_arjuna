d488c4aea405647bbc5c0fc901100b9b8e9745a7
Imam Asari <imam.as@treeclouds.com>
2023-07-24 10:30:17 +0700

    render list percipio data

---
+++ lib/data/percipio_data.dart
@@ -0,0 +1,83 @@
+const percipioData = [
+  {
+    "id": 1,
+    "title": "Skill Areas",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/subject/images/saved/17ad2c50-dcc2-11e6-b53c-c991ff5fe8a1.png",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/library?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 2,
+    "title": "Certification",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/subject/images/saved/cab37a10-2b74-11e7-bbbf-3b6e78cef3cf.jpg",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/certifications?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 3,
+    "title": "Aspire Journey",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/channel/images/saved/f5620461-72c3-414b-a253-e0ff2dec74a4/d63e2fa9-f6d0-4292-8be1-92f23b19c5b1.jpg",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/journey?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 4,
+    "title": "Codecademy",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/subject/images/saved/329a3779-0db9-49ab-b868-df8f4f1c8a9e/78799277-8c1c-41e1-850e-bc5de15455f9.jpg",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/library/aeed4862-eee4-485a-9ec9-598333841358?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 5,
+    "title": "Live Course Calendar",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/public/images/Livecourse_Banner.png",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/liveCourses?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 6,
+    "title": "Skill Benchmarks",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/public/images/skill-benchmark-image.png",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/skill-benchmarks?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 7,
+    "title": "Skillsoft Bootcamps",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/subject/images/saved/f13aea0b-8359-4a05-950c-3bd4c75e78a6/c7779d2f-920c-4058-bfe1-430c40a5a976.jpg",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/library/9924fa13-0e97-4976-b44c-5f0637edf4c9?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 8,
+    "title": "Skillsoft Leadercamps",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/subject/images/saved/43e1f155-4853-4b9f-9966-b909dfdc37ab/d1a81cd6-be99-47ed-8c06-5f238ca10bf2.jpg",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/library/dffb79e2-6fe5-4552-8b19-c147e4c76f2d?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 9,
+    "title": "Skillsoft Live Events",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/subject/images/saved/5983c45b-e18f-48a1-a034-8030a026dc28/4e829d1e-d5ad-4121-aa80-ca63c86f073d.jpg",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/library/3452cf1f-2317-4e36-ade0-a4bb384f9d42?preventAppDownload=true&chromeless=true"
+  },
+  {
+    "id": 10,
+    "title": "Skillsoft’s The Edge Podcast",
+    "description": "",
+    "imageUrl": "https://cdn2.percipio.com/public/c/subject/images/saved/88ba88bc-bff9-4681-87ed-7928efb93458/0120bae3-a756-47a3-acef-33154bf5ac89.JPG?width=556",
+    "category": "Certification",
+    "link": "https://netpolitan-eval.percipio.com/library/e19fb4f4-0d2f-43c6-b258-e0c4d96c1bea?preventAppDownload=true&chromeless=true"
+  }
+];
+
--- lib/percipio_access.dart
+++ lib/percipio_access.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:arjuna/data/percipio_data.dart' as data;
 
 class PercipioAccess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Arjuna 2'),
+        title: Text('Percipio Access'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back'),
-        ),
+      body: ListView.builder(
+        itemCount: data.percipioData.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Image.network(data.percipioData[index]['imageUrl'] as String),
+            title: Text(data.percipioData[index]['title'] as String),
+            onTap: () {
+              _onListItemTap(context, index);
+            },
+          );
+        },
       ),
     );
+  }
+
+  // Define the _onListItemTap method or remove the call if it's not needed
+  void _onListItemTap(BuildContext context, int index) {
+    // Your logic here
   }
 }
